c77dfb6c26d10c9cec0c55e02ba82f4be79a959a
furkanKrdmr <karademirfurkan69@gmail.com>
2021-04-14 15:41:09 +0200

    Made link with Jquery and slink.JS finished the display of the color choice menu

---
+++ public/assets/script/script.js
@@ -0,0 +1,28 @@
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    accessibility: true,
+    centerPadding: '50px',
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
