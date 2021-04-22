a6da4d633162d616077699d4576254e11ec03cf6
furkanKrdmr <karademirfurkan69@gmail.com>
2021-04-22 13:26:28 +0200

    Updated the color choice slider, made few sliders for accesories, made the step by step process(few sliders into one slider), separated the css created a _Customer.css, created view for custom with his Controller and Manager.

---
+++ src/Controller/CustomController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class CustomController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function custom()
+    {
+        return $this->twig->render('Custom/custom.html.twig');
+    }
+}
---
+++ src/Model/CustomManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class CustomManager extends AbstractManager
+{
+    public const TABLE = 'article';
+}
