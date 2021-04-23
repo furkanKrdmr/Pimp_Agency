aa97b641faf7e9745271bf851604a1f175d2dff4
furkanKrdmr <79790959+furkanKrdmr@users.noreply.github.com>
2021-04-23 09:44:20 +0200

    Us furkan (#28)
    * Updated all the files + composer
    
    * Updated the color choice slider, made few sliders for accesories, made the step by step process(few sliders into one slider), separated the css created a _Customer.css, created view for custom with his Controller and Manager.
    
    * deleted commented code in _Custom.css
    
    * To merge into dev

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,18 +1,239 @@
-   //Début js slider moto
+/*  début Custom.html.twig fait par furkan */
+$(document).ready(function () {
+  $(".container").slick({
+    dots: false,
+    infinite: false,
+    draggabele: false,
+    swipe: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+        },
+      },
+    ],
+  });
 
-$('.slider-moto').slick
-({
+  $(".slider-1").slick({
     dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'});
-  
-    //fin js slider moto
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+  $(".slider-2").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
 
-    //Début du slider choix de couleur fait par furkan
-$('.slider').slick
-({
+  $(".slider-3").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+
+  $(".slider-4").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+
+  $(".slick-next").click(function () {
+    if ($("#title").text().indexOf("couleur")) {
+      $("#title").text("Choisissez votre rétroviseurs");
+    }
+  });
+});
+
+function imgFullYellow() {
+  let imageId = document.getElementById("image");
+  if (imageId.src !== undefined) {
+    imageId.src = "../assets/Images/Jaune/moto-Y-B-B.jpg";
+  }
+}
+
+function imgFullRed() {
+  let imageId = document.getElementById("image");
+  if (imageId.src !== undefined) {
+    imageId.src = "../assets/Images/Jaune/moto-R-B-B.jpg";
+  }
+}
+
+function imgFullGreen() {
+  let imageId = document.getElementById("image");
+  if (imageId.src !== undefined) {
+    imageId.src = "../assets/Images/Jaune/moto-G-B-B.jpg";
+  }
+}
+
+function imgFullYellow() {
+  let imageId = document.getElementById("image");
+  if (imageId.src !== undefined) {
+    imageId.src = "../assets/Images/Jaune/moto-Y-B-B.jpg";
+  }
+}
+function imgFullBlue() {
+  let imageId = document.getElementById("image");
+  if (imageId.src !== undefined) {
+    imageId.src = "../assets/Images/Jaune/moto-Bl-B-B.jpg";
+  }
+}
+
+function imgYellowBlue() {
+  let imageId = document.getElementById("image");
+  if (imageId.getAttribute("src") == "../assets/Images/Jaune/moto-Y-B-B.jpg") {
+    imageId.src = "../assets/Images/Jaune/moto-Y-Bl-Bl.jpg";
+  }
+}
+/*  fin Custom.html.twig fait par furkan */
+//Début js slider moto
+
+$(".slider-moto").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: "linear",
+});
+
+//fin js slider moto
+
+//Début du slider choix de couleur fait par furkan
+$(".slider").slick({
   dots: true,
   infinite: false,
   speed: 300,
@@ -25,55 +246,54 @@ $('.slider').slick
         slidesToShow: 3,
         slidesToScroll: 3,
         infinite: true,
-        dots: true
-      }
+        dots: true,
+      },
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
-      }
+        slidesToScroll: 2,
+      },
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }
-  ]});
-
-  // début slider vitrine
-  
-  $('.slider-vitrine').slick({
-    centerMode: true,
-    centerPadding: '0px',
-    slidesToShow: 2,
-    autoplay: true,
-  autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
-          slidesToShow: 3
-        }
+        slidesToScroll: 2,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '0px',
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
+    },
+  ],
+});
 
- 
+// début slider vitrine
 
-  //fin slider vitrine
+$(".slider-vitrine").slick({
+  centerMode: true,
+  centerPadding: "0px",
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "0px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "0px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+//fin slider vitrine
