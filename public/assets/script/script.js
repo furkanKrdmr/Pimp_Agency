35941cc17e226a385c8418e8e5c2d9f188bbf338
jouannetaud <remijouannetaud10@gmail.com>
2021-04-23 10:13:57 +0200

    create files navbar css + html

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -10,43 +10,12 @@ $('.slider-moto').slick
   
     //fin js slider moto
 
-    //Début du slider choix de couleur fait par furkan
-$('.slider').slick
-({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }
-  ]});
+
 
   // début slider vitrine
   
   $('.slider-vitrine').slick({
+    arrows: false,
     centerMode: true,
     centerPadding: '0px',
     slidesToShow: 2,
@@ -59,7 +28,7 @@ $('.slider').slick
           arrows: false,
           centerMode: true,
           centerPadding: '0px',
-          slidesToShow: 3
+          slidesToShow: 2
         }
       },
       {
